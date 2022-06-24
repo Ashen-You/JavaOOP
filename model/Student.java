fcb23eff5cc5983b4e88b20cdf9e4a9d2f0409d6
Travis You <1244098000@qq.com>
2022-06-24 09:31:52 +0800

    docs(Student): add constructor method and get/set

--- model/Student.java
+++ model/Student.java
@@ -1,5 +1,47 @@
 package model;
 
 public class Student {
+    private String name;
+    private int age;
+    private int gender;
+    private int studentNumber;
 
+    public Student(String name, int age, int gender, int studentNumber) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.studentNumber = studentNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(int studentNumber) {
+        this.studentNumber = studentNumber;
+    }
 }
