0d1a57fb23487fbc304f7b982e93d46314a73c38
Travis You <1244098000@qq.com>
2022-06-24 09:38:15 +0800

    docs(Main): use Student in Main

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
-public class Main {
+import model.Student;
 
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Travis You", 22, '1', 35);
+        System.out.println("Student Name:   " + student.getName());
+        System.out.println("Student Age:    " + student.getAge());
+        System.out.println("Student Gender: " + (student.getGender() == 1 ? "male": "female"));
+        System.out.println("Student ID NO:  " + student.getStudentNumber());
+    }
 }
